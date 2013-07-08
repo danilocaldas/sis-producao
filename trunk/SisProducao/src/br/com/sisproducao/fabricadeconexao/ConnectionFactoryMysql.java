33728f5f79b7ac3f7ef0959966120e4c9c13a0ae
dmcmaumau@gmail.com <dmcmaumau@gmail.com@214feebb-4a0f-ae71-5bbe-66ab7bd69435>
2013-07-08 22:56:39 +0000

    Importando fase inicial

---
+++ trunk/SisProducao/src/br/com/sisproducao/fabricadeconexao/ConnectionFactoryMysql.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.sisproducao.fabricadeconexao;
+
+import java.sql.Connection;
+
+/**
+ *
+ * @author Danilo
+ */
+public class ConnectionFactoryMysql {
+    
+    public static Connection con;
+    
+}
